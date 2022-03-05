0ba239fee1306d6d0fe8648ec05fa761cedc6b2a
MarlaSantos <72895099+marlasantos24@users.noreply.github.com>
2022-03-04 23:30:34 -0500

    ListView_AdaptadorSimple

--- app/src/main/java/com/example/ejemplolistview/MainActivity.kt
+++ app/src/main/java/com/example/ejemplolistview/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.ejemplolistview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,10 +15,15 @@ class MainActivity : AppCompatActivity() {
         var frutas:ArrayList<String> = ArrayList()
         frutas.add("Manzana")
         frutas.add("Durazno")
-        frutas.add("Manzaca")
-        frutas.add("Manzaca")
-        frutas.add("Manzaca")
+        frutas.add("Plátano")
+        frutas.add("Sandía")
+        frutas.add("Fresa")
 
-
+        val lista = findViewById<ListView>(R.id.lista)
+        val adaptador = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, frutas)
+        lista.adapter = adaptador
+        lista.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
+            Toast.makeText(this, frutas.get(position), Toast.LENGTH_LONG).show()
+        }
     }
 }
